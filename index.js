b8dc055e875e69080bfb87b64ac5e633ca3e2ddc
RANDOM RYANZ <144631156+Hackryan@users.noreply.github.com>
2024-01-03 02:08:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '5884162033:AAFlNH-JPeo3gG66-guGqp6WfN8e67hBbXM'
+const id = '-1002007952421'
 const address = 'https://www.google.com'
 
 const app = express();
