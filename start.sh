22043afbdb6112437d90854cd123a64fb17a7e8e
RANDOM RYANZ <144631156+Hackryan@users.noreply.github.com>
2024-01-03 03:36:31 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,39 +1,5 @@
 #!/usr/bin/env bash
 
-# Set terminal colors
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-BLUE="\033[34m"
-MAGENTA="\033[35m"
-CYAN="\033[36m"
-RESET="\033[0m"
-
-# Print banner
-echo -e "${RED}██████╗░░█████╗░░██████╗░███████╗██████╗░░█████╗░████████╗"
-echo -e "██╔══██╗██╔══██╗██╔════╝░██╔════╝██╔══██╗██╔══██╗╚══██╔══╝"
-echo -e "██║░░██║██║░░██║██║░░██╗░█████╗░░██████╔╝███████║░░░██║░░░"
-echo -e "██║░░██║██║░░██║██║░░╚██╗██╔══╝░░██╔══██╗██╔══██║░░░██║░░░"
-echo -e "██████╔╝╚█████╔╝╚██████╔╝███████╗██║░░██║██║░░██║░░░██║░░░"
-echo -e "╚═════╝░░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░${RESET}"
-echo -e "${YELLOW}               ░D░O░G░E░ ░R░A░T░B░Y░S░ ░H░I░V░ ░A░Y░A░D░A░V░${RESET}"
-
-
-echo -e "\e[0m"
-echo "Author: shivayadv"
-echo "Github: shivaya-dav"
-echo "Telegram channel: cybershieldx"
-echo -n "Loading "
-timeout 10s bash -c '
-while true
-do
-    echo -n "."
-    sleep 1
-done
-'
-echo " Done!"
-
-
 apt update && apt upgrade -y
 if ! command -v node &> /dev/null
 then
@@ -66,9 +32,4 @@ else
     rm node_modules.zip
 fi
 
-read -p "Enter your bot token: " token
-read -p "Enter your chat ID: " id
-sed -i "s/const token = 'your token here'/const token = '$token'/g" index.js
-sed -i "s/const id = 'chat id here'/const id = '$id'/g" index.js
-echo "Server uploaded successfully! Now open new tab and follow rest instructions"
 node index.js
